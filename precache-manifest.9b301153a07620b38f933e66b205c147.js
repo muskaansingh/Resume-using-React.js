66bc47d3e5138ebd3b3f0704e8834df487672b1b
Muskaan Singh <muskaansingh235@gmail.com>
2020-08-28 02:14:00 +0530

    Updates

---
+++ precache-manifest.9b301153a07620b38f933e66b205c147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb16804f0b1befd000592ecbcb87f27",
+    "url": "/Resume-using-React/index.html"
+  },
+  {
+    "revision": "6bbb9a9fd2c99e261643",
+    "url": "/Resume-using-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "204448f4c1caa1a92f2c",
+    "url": "/Resume-using-React/static/js/2.215a2eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Resume-using-React/static/js/2.215a2eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbb9a9fd2c99e261643",
+    "url": "/Resume-using-React/static/js/main.7b389d66.chunk.js"
+  },
+  {
+    "revision": "4c7c49de06873edb30b7",
+    "url": "/Resume-using-React/static/js/runtime-main.6b291a83.js"
+  },
+  {
+    "revision": "ea71f741e0e19fe20cc75098ca251caa",
+    "url": "/Resume-using-React/static/media/mus.ea71f741.jpeg"
+  }
+]);
